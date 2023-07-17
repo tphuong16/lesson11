54ed5ef6c6a6bab61d1d33cca3295eb172fdd43b
tphuong16 <132880191+tphuong16@users.noreply.github.com>
2023-07-17 20:52:41 +0700

    Create action-create.php

---
+++ product/action-create.php
@@ -0,0 +1,13 @@
+<?php
+    require_once 'pdo.php';
+    require_once "../category/pdo.php";
+    $productConnection = new ProductConnection();
+    $data = [
+        'prodId' => '',
+        'prodName' => $_POST['name'],
+        'prodPrice' => $_POST['price'],
+        'cateId' => $_POST['cateId']
+    ];
+    $productConnection->createNewProdData($data);
+    header("Location: http://localhost/lesson11/product/index.php");
+?>
